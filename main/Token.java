f25e8c1e067e270daf55fb04f276bae9db0964e5
Sajith Dharmasena <sajith@berkeley.edu>
2017-06-29 23:39:40 -0700

    Recognize '\', '\Q', and '\E'

--- main/Token.java
+++ main/Token.java
@@ -26,15 +26,52 @@ class Token {
 	}
 
 	/* RETURNS an ArrayList of tokens that represent the input PATTERN. */
-	static ArrayList<Token> tokenize (String pattern) throws InvalidRegexException{
+	static ArrayList<Token> tokenize (String pat) throws InvalidRegexException{
+		ArrayList<Character> pattern = new ArrayList<Character>();
+		for (int i = 0; i < pat.length(); i++) 
+			pattern.add(pat.charAt(i));
 		ArrayList<Token> tokens = new ArrayList<Token>();
 
-		for (int i = 0; i < pattern.length(); i++) {
-			Token tok = new Token(pattern.charAt(i));
-			tokens.add(tok);
+		while (!pattern.isEmpty()) {
+			char c = pattern.remove(0);
+			switch(c) {
+				case '\\':
+					tokenizeEscapes(pattern, tokens);
+					break;
+				default:
+					tokens.add(new Token(c));
+			}
 		}
 
 		return tokens;
+	}
+
+	private static void tokenizeEscapes (ArrayList<Character> pattern, ArrayList<Token> tokens) {
+		if (pattern.isEmpty())
+			throw new InvalidRegexException("missing character after '\\'");
+
+		char c = pattern.remove(0);
+		switch (c) {
+			case 'Q':
+				while (!pattern.isEmpty()) {
+					char cc = pattern.remove(0);
+					if (cc == '\\') {
+						if (!pattern.isEmpty() && pattern.get(0) == 'E') {
+							pattern.remove(0);
+							break;
+						} else {
+							tokens.add(new Token(cc));
+						}
+					} else {
+						tokens.add(new Token(cc));
+					}
+				}
+				break;
+			case 'E':
+				throw new InvalidRegexException("missing '\\Q' before '\\E'");
+			default:
+				tokens.add(new Token(c));
+		}
 	}
 }
 
